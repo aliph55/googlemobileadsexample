f82488e05d80cafdca4a2dfafc9d356b6d7f9e5e
Ali Pourhassan <67645398+aliph55@users.noreply.github.com>
2025-04-03 22:20:23 +0300

    master

--- App.jsx
+++ App.jsx
@@ -1,59 +1,14 @@
-import React, { useEffect } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
-import { RewardedAd, RewardedAdEventType, TestIds } from 'react-native-google-mobile-ads';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-const adUnitId = TestIds.REWARDED; // "ca-app-pub-3940256099942544/5224354917"
-
-const rewarded = RewardedAd.createForAdRequest(adUnitId, {
-  requestNonPersonalizedAdsOnly: true,
-});
-
-export default function App() {
-  useEffect(() => {
-    const unsubscribeLoaded = rewarded.addAdEventListener(RewardedAdEventType.LOADED, () => {
-      rewarded.show();
-    });
-
-    const unsubscribeEarned = rewarded.addAdEventListener(
-      RewardedAdEventType.EARNED_REWARD,
-      (reward) => {
-        console.log(`Kullanıcı ${reward.amount} ${reward.type} kazandı!`);
-      }
-    );
-
-    const unsubscribeError = rewarded.addAdEventListener(RewardedAdEventType.ERROR, (error) => {
-      console.log('Reklam yüklenemedi:', error);
-    });
-
-    rewarded.load();
-
-    return () => {
-      unsubscribeLoaded();
-      unsubscribeEarned();
-      unsubscribeError();
-    };
-  }, []);
-
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Button
-        title="Rewarded Reklam Göster"
-        onPress={() => {
-          if (rewarded.loaded) {
-            rewarded.show();
-          } else {
-            rewarded.load();
-          }
-        }}
-      />
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
